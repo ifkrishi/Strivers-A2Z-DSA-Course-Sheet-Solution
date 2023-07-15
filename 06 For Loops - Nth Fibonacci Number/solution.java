6a1ae04dfdac4a44a5392bd13cc7a0090cb43ba6
Himanshi Agrawal <99007273+ifkrishi@users.noreply.github.com>
2023-07-15 15:02:32 +0530

    Create solution.java

---
+++ 06 For Loops - Nth Fibonacci Number/solution.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Solution {
+	public static int FibonacciSeries(int n){
+		int a = 0, b = 1, c;
+        if (n == 0)
+            return a;
+        for (int i = 2; i <= n; i++) {
+            c = a + b;
+            a = b;
+            b = c;
+        }
+        return b;
+	}
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		System.out.print(FibonacciSeries(n));
+	}
+
+}
